5c4ac906ca1d7fc91c2601501317e2e9f0c53dc0
JBRS307 <jakub.rekas@hotmail.com>
2020-12-13 13:20:48 +0100

    day8.2 done, day8.1 updated

--- day8/day8.2.py
+++ day8/day8.2.py
@@ -1,8 +1,12 @@
+from copy import deepcopy
+
 def findLoop(arr):
     checked = []
     i = 0
     acc = 0
     while i not in checked:
+        if i >= len(arr):
+            return acc, True
         checked.append(i)
         if arr[i][0] == 'nop':
             i+=1
@@ -14,7 +18,21 @@ def findLoop(arr):
         if arr[i][0] == 'jmp':
             i += int(arr[i][1])
             continue
-    return acc
+    return acc, False
+
+def findError(arr):
+    for i in range(len(arr)):
+        newArr = deepcopy(arr)
+        if newArr[i][0] == 'acc':
+            continue
+        if newArr[i][0] == 'jmp':
+            newArr[i][0] = 'nop'
+        elif newArr[i][0] == 'nop':
+            newArr[i][0] = 'jmp'
+        res, valid = findLoop(newArr)
+        if valid == True:
+            return res
+
 
 #===============================================================================
 inputFile = open("input.txt", 'r')
@@ -24,4 +42,4 @@ for line in inputFile:
     arr.append(line)
 inputFile.close()
 
-print(findLoop(arr))
+print(findError(arr))
